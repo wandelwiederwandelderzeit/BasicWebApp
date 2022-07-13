d0b9a311e3e3b3c78868ae66e8ea5d283c0c5aeb
ge65mof <frederike.wandel@tum.de>
2022-07-13 13:25:05 +0200

    1st calc

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,16 +6,32 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "riki";
-        } else { // TODO extend the programm here
-            return "";
+            return "riki";
+        } else if (query.contains("plus")) {
+            // query = "what is 3 plus 3"
+            String[] wordList = query.split(" ");
+            int sum = 0;
+            for (String word : wordList) {
+                try {
+                    int intOfWord = Integer.parseInt(word);
+                    sum += intOfWord;
+                } catch (Exception e) {
+
+                }
+
+                //Integer.parseInt("myString 2");
+                //String three = query.substring(2, 1);
+                // int result = Integer.parseInt(three);
+                return String.valueOf(sum);
+            }
+
         }
-        //HelloWorld
+        return "";
     }
 }
